ca1824b71fb5daac11fe0eb5b6c02f9ae78ae8c4
stv <stv@localhost.localdomain>
2019-07-29 15:28:11 +0300

    8080

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 func main() {
 
 	/* пример запросов:
-	1. string	http://127.0.0.1:1323/redir/string?shrt=https://302.su/r/8y
-	2. json	http://127.0.0.1:1323/redir/json?shrt=https://302.su/r/8y
+	1. string	http://127.0.0.1:8080/redir/string?shrt=https://302.su/r/8y
+	2. json	http://127.0.0.1:8080/redir/json?shrt=https://302.su/r/8y
 	*/
 	e := echo.New()
 	e.GET("/tst", handlers.HWorld)
 	e.GET("/redir/:data", handlers.Redir)
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
